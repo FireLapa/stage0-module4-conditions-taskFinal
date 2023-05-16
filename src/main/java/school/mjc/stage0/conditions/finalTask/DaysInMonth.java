640c8dcb481183ab5dfa793c59c1c4efb1c48780
Lapa <lapabig@gmail.com>
2023-05-16 19:50:47 +0600

    Updated

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,37 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        int leap;
+        if (year %4 == 0 && year % 100 != 0 || year%400==0){
+            leap = 1;
+        } else {
+            leap = 2;
+        }
+        switch (month) {
+            case 2:
+                if (leap != 1) {
+                    System.out.println("28");
+                } else {
+                    System.out.println("29");
+                }
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 9:
+            case 11:
+                System.out.println("31");
+                break;
+            case 4:
+            case 6:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("30");
+                break;
+            default:
+                System.out.println("invalid date");
+        }
     }
 }
